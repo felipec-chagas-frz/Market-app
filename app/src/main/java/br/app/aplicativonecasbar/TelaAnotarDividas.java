4a22bf4fc7580fa289b3b5cd3bca4d41703225a8
frz-developer <frz.github@gmail.com>
2024-01-10 16:42:44 -0300

    Aplicativo Mercado

---
+++ app/src/main/java/br/app/aplicativonecasbar/TelaAnotarDividas.java
@@ -0,0 +1,14 @@
+package br.app.aplicativonecasbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TelaAnotarDividas extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tela_anotar_dividas);
+    }
+}
